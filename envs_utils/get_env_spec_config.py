41a01040d00cd59e19e22bba3f9e0e47af66752c
pedramrabiee <pedram.rabiee@gmail.com>
2023-10-20 09:04:08 -0400

    Moved methods to make_setup

--- envs_utils/get_env_spec_config.py
+++ envs_utils/get_env_spec_config.py
@@ -7,14 +7,14 @@ def get_env_spec_config(train_env):
             raise NotImplementedError
     elif train_env['env_collection'] == 'safety_gym':
         if train_env['env_id'] == 'Point':
-            from envs_utils.safety_gym.point_robot_configs import env_config
-            from envs_utils.safety_gym.point_robot_configs import engine_config
+            from envs_utils.safety_gym.point.point_configs import env_config
+            from envs_utils.safety_gym.point.point_configs import engine_config
             env_config.update(engine_config)
     elif train_env['env_collection'] == 'misc':
         if train_env['env_id'] == 'cbf_test':
-            from envs_utils.test_env.test_env_configs import env_config
-        if train_env['env_id'] == 'multi_mass_dashpot':
-            from envs_utils.test_env.multi_m_dashpot_configs import env_config
+            from envs_utils.misc_env.cbf_test.cbf_test_configs import env_config
+        if train_env['env_id'] == 'multi_dashpot':
+            from envs_utils.misc_env.multi_dashpot.multi_dashpot_configs import env_config
     else:
         raise NotImplementedError
     return env_config
--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from attrdict import AttrDict
 from utils.seed import set_seed
 from utils.process_observation import get_obsproc_cls
 from utils.custom_plotter import get_custom_plotter_cls
-from rewards.get_reward_gen import get_reward_gen
+from utils.get_reward_gen import get_reward_gen
 from envs_utils.get_env_info import get_env_info
 from logger import logger
 
 
 def make_setup(env_nickname, agent, load_config_path=None):
     env_info = get_env_info(env_nickname)
-
-    return AttrDict(agent=agent,
+    setup = AttrDict(agent=agent,
                     train_env=dict(env_id=env_info['env_id'],
                                    env_collection=env_info['env_collection'],
                                    env_nickname=env_nickname),
@@ -23,6 +22,12 @@ def make_setup(env_nickname, agent, load_config_path=None):
                     # if you wish to load the env_config file. otherwise, set it None
                     )
 
+    setup['custom_plotter_cls'] = get_custom_plotter_cls(setup['train_env'])
+    setup['reward_gen'] = get_reward_gen(setup['train_env'])
+    setup['obs_proc_cls'] = get_obsproc_cls(setup['train_env'])
+
+    return setup
+
 if __name__ == "__main__":
 
     # See envs_utils.get_env_info for a list of acceptable environment nicknames
@@ -31,10 +36,6 @@ if __name__ == "__main__":
 
     # Set random.seed, generate default_rng, torch.manual_seed, torch.cuda.manual_seed_all
     seed = set_seed()
-
-    setup['reward_gen'] = get_reward_gen(setup['train_env'])
-    setup['obs_proc_cls'] = get_obsproc_cls(setup['train_env'])
-    setup['custom_plotter_cls'] = get_custom_plotter_cls(setup['train_env'])
 
     root_dir = os.path.dirname(os.path.realpath(__file__))
 
